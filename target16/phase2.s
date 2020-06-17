1208a2f49e3e48760d406c59622e6cd0065a1a35
liho2210 <liam.hollins@colorado.edu>
2020-06-17 19:17:56 +0000

    first commit

---
+++ target16/phase2.s
@@ -0,0 +1,2 @@
+mov $0x49308bb9, %rdi
+ret
---
+++ target16/phase2assem.asm
@@ -0,0 +1,9 @@
+
+phase2.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 b9 8b 30 49 	mov    $0x49308bb9,%rdi
+   7:	c3                   	retq   
